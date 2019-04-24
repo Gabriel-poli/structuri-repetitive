a30ad88424a13fcb5ab360c4531dcdfddeaf1a54
Gabriel-poli <floricel_gabi@yahoo.com>
2019-04-24 17:07:07 +0300

    Exercitiu 2 numar par numar impar - final

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,20 +2,21 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here Exercitiul 3 rev.2 - final
-        int N = 6;
-        int contor = 0;
-        while (N != 1) {
-            if (N % 2 == 1) {
-                N = N * 3 + 1;
-                contor++;
-            } else {
-                N = N / 2;
-                contor++;
+        // write your code here Exercitiul 3
+        int[] Karray = {5, 6, 2, 13, 8, 14, 99};
+        int numar = -32000;
+        for (int i = 0; i < Karray.length; i++)
+            if (Karray[i] % 2 == 1) {
+                if (Karray[i] > numar) {
+                    numar = Karray[i];
+                } else {
+                }
             }
-
-
+        if (numar == -32000) {
+            System.out.println("NO");
+        } else {
+            System.out.println("Cel mai mare numar impar este:  " + numar);
         }
-        System.out.println(contor);
+
     }
 }

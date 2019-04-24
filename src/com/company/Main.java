9591fa29f2a81d3967630d5586ae9c0994fe1279
Gabriel-poli <floricel_gabi@yahoo.com>
2019-04-24 17:37:40 +0300

    Exercitiu 7

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,15 +2,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here Exercitiul 6
-        int n = 5;
-        for (int i = 0; i <= n; i++) {
-            if (i % 2 == 1) {
-                System.out.println(i);
-            } else {
-
-
-            }
+        // write your code here Exercitiul 7
+        int n = 2;
+        int calcul = 1;
+        while (n!= 0) {
+            calcul=calcul*3;
+            n--;
+        }
+    System.out.println(2*calcul);
         }
     }
-}

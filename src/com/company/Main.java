2852e1ecb92ad907822b56276c91ba1474f745f6
Gabriel-poli <floricel_gabi@yahoo.com>
2019-04-24 16:43:33 +0300

    Exercitiu 2 numar par numar impar - final

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,16 +2,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here Exercitiul 1
+        // write your code here Exercitiul 3 rev.2 - final
+        int N = 6;
+        int contor = 0;
+        while (N != 1) {
+            if (N % 2 == 1) {
+                N = N * 3 + 1;
+                contor++;
+            } else {
+                N = N / 2;
+                contor++;
+            }
 
-        int N = 9;
-        if (N % 2 == 1) {
-            System.out.print("Numarul este impar, noul numar este:");
-            N = N * 3 + 1;
-        } else {
-            System.out.print("Numarul este par, noul numar este: ");
-            N = N / 2;
+
         }
-        System.out.print(N);
+        System.out.println(contor);
     }
 }

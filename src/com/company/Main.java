f1ee7f412ea185486b45e68b49982652b4bfc96a
Gabriel-poli <floricel_gabi@yahoo.com>
2019-04-24 17:26:35 +0300

    Exercitiu 5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,18 +2,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here Exercitiul 4
-        int[] vector = {5, 3, 14, 17, 8, 5, 23};
-        int nrpar = 0;
-        int nrimpar = 0;
-        for (int i = 0; i < vector.length; i++) {
-            if (vector[i] % 2 == 0) {
-                nrpar++;
-            } else {
-                nrimpar++;
-            }
+        // write your code here Exercitiul 5
+        int n = 7;
+        for (int i = n; i > 0; i--) {
+            System.out.println(i);
 
         }
-        System.out.println("Exista " + nrpar + " numere pare si " + nrimpar + " numere impare");
     }
 }

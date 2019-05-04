0705a5ee241bee750eff09cf41f2503d29d26d9d
Gabriel-poli <lovelar22>
2019-05-04 20:13:37 +0300

    Exercitiul 11 if else si switch

---
+++ src/com/company/Exercitiul12.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Exercitiul12 {
+    public static void main(String[] args) {
+        int n = 7;
+        switch (n) {
+            case 12:
+                System.out.println();
+            case 1:
+                System.out.println();
+            case 2:
+                System.out.println("iarna");
+                                break;
+            case 3:
+                System.out.println();
+            case 4:
+                System.out.println();
+            case 5:
+                System.out.println("primavara");
+                break;
+            case 6:
+                System.out.println();
+            case 7:
+                System.out.println();
+            case 8:
+                System.out.println("vara");
+                break;
+            case 9:
+                System.out.println();
+            case 10:
+                System.out.println();
+            case 11:
+                System.out.println("toamna");
+                break;
+            default:
+                System.out.println("input invalid");
+        }
+    }
+}

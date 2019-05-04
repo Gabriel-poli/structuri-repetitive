e61b563dfde405a84d4cc4c68d97778ee38a8617
Gabriel-poli <lovelar22>
2019-05-04 20:19:59 +0300

    Exercitiul 13 if

---
+++ src/com/company/Exercitiul13.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Exercitiul13 {
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i = 0; i < n; i++) {
+            System.out.print("go ");
+
+        }
+        System.out.print("gata");
+    }
+}

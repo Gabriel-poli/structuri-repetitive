3784a90abf56e936a5bbde088d2cdd96435aceb4
Gabriel-poli <lovelar22>
2019-05-04 19:20:26 +0300

    Exercitiul 10 Suma numere pare vector

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,24 @@
 package com.company;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here Exercitiul 9
-        int n = 5;
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j <= i; ++j){
-                System.out.print(j);
+        // Exercitiul 10 Suma numere pare vector
+        int[] unArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        int contor = 0;
+        int suma = 0;
+        for (int i = 0; i < unArray.length; i++) {
+            if (unArray[i] % 2 == 0) {
+                suma = suma + unArray[i];
+                contor++;
             }
-            System.out.println();
+            if (contor == 5) {
+                break;
+            }
+
 
         }
+        System.out.println("Suma primelor 5 numere pare din vector este: " + suma);
+
     }
 }

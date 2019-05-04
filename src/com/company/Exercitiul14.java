de1a480ba2be5df69f6437eac14eefbaa5b4585b
Gabriel-poli <lovelar22>
2019-05-04 20:22:47 +0300

    Exercitiul 14 bucla infinita

---
+++ src/com/company/Exercitiul14.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Exercitiul14 {
+    public static void main(String[] args) {
+        int n = 6;
+        while (n != 0) {
+            System.out.println("going");
+            n--;
+        }
+    }
+}
